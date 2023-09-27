af36acfbc0cd7c1511f85ebe612c0a18b76bb18d
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-09-27 18:37:28 +0300

    HW 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,7 @@ const galleryEl = document.querySelector(".gallery");
 
 const markup = galleryItems
   .map(({ preview, original, description }) => {
-    return `<li class="gallery"><a class="gallery__item" href="${original}">
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
 		<img class="gallery__image" src="${preview}" alt="${description}" />
 	</a></li>`;
   })
