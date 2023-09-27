11abc40e3e7bb199917de76a4015cece1dc6b140
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-09-27 18:41:48 +0300

    HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -53,65 +53,7 @@ function onClick(event) {
 }
 
 
-// import { galleryItems } from "./gallery-items.js";
 
-// console.log(galleryItems);
-
-// const galleryEl = document.querySelector(".gallery");
-
-// const renderListGallery = (makeImageItems) =>
-//   makeImageItems.reduce(
-//     (acc, { preview, original, description }) =>
-//       acc +
-//       `<li class="gallery__item"><a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"/>
-//   </a>
-// </li>`,
-//     ""
-//   );
-
-// galleryEl.insertAdjacentHTML("afterbegin", renderListGallery(galleryItems));
-
-// /*событие*/
-
-// galleryEl.addEventListener("click", onAllGalleryClick);
-
-// function onAllGalleryClick(e) {
-//   const imageLink = e.target.dataset.source;
-
-//   e.preventDefault();
-
-//   if (!e.target.classList.contains("gallery__image")) {
-//     return;
-//   }
-//   const makeImage = `<img
-//       src="${imageLink}"
-//       width="800"
-//       height="600"
-//       >`;
-
-//   const instance = basicLightbox.create(makeImage, {
-//     onShow: () => {
-//       window.addEventListener("keydown", onEscKeyClick);
-//     },
-//     onClose: () => {
-//       window.removeEventListener("keydown", onEscKeyClick);
-//     },
-//   });
-//   instance.show();
-
-//   function onEscKeyClick(e) {
-//     const ESC_KEY_CODE = "Escape";
-//     const isEscKey = e.code === ESC_KEY_CODE;
-//     if (isEscKey) {
-//       instance.close();
-//     }
-//   }
-// }
 
 
 
